496e86d436f109ba7d2dce17d951208791062222
owaisk23 <owais1566@gmail.com>
2026-03-25 19:08:54 +0500

    added dynamic list

--- MobAppDevelopments/firstproject/lib/dynamicList.dart
+++ MobAppDevelopments/firstproject/lib/dynamicList.dart
@@ -36,23 +36,89 @@ class _DynamicListState extends State<DynamicList> {
       "name": "Moin Bhai",
     }
   ];
+
+   var Cars = [
+    {
+      "name": "BMW",
+      "model": "X5",
+      "price": 50000,
+      "color": "Black",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    },
+    {
+      "name": "Audi",
+      "model": "A6",
+      "price": 60000,
+      "color": "White",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    },
+    {
+      "name": "Mercedes",
+      "model": "C-Class",
+      "price": 70000,
+      "color": "Silver",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    },
+    {
+      "name": "Toyota",
+      "model": "Camry",
+      "price": 30000,
+      "color": "Blue",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    },
+    {
+      "name": "Honda",
+      "model": "Accord",
+      "price": 25000,
+      "color": "Red",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    }
+    ];
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dynamic List"),
+        title: Text('Dynamic List'),
+        backgroundColor: Colors.black87,
+        foregroundColor: Colors.white,
+        centerTitle: true,
       ),
-      drawer: drawerSidebar.DrawerSide(),
-       body: ListView.builder(
-        itemCount: Users.length,
+      body:Center(
+        child:  ListView.builder(
+        itemCount: Cars.length,
         itemBuilder: (context, index){
-          return ListTile(
-            title: Text('${Users[index]['name']}'),
-            subtitle: Text('This is our BHAI users!!'),
-            leading: Icon(Icons.person),
-            trailing: Icon(Icons.arrow_forward_ios),
+          return Padding(
+            padding: EdgeInsets.all(10),
+            child: ListTile(
+            title: Text('${Cars[index]['name']}'),
+            subtitle: Text('Car Model: ${Cars[index]['model']} \nCar Color: ${Cars[index]['color']}'),
+            leading: Image.network('${Cars[index]['image']}'),
+            trailing: Text('Car Price: ${Cars[index]['price']}', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),)
+          ),
           );
         },
+      )
       ),
+      drawer: drawerSidebar.DrawerSide(),
     );
+
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text("Dynamic List"),
+  //     ),
+  //     drawer: drawerSidebar.DrawerSide(),
+  //      body: ListView.builder(
+  //       itemCount: Users.length,
+  //       itemBuilder: (context, index){
+  //         return ListTile(
+  //           title: Text('${Users[index]['name']}'),
+  //           subtitle: Text('This is our BHAI users!!'),
+  //           leading: Icon(Icons.person),
+  //           trailing: Icon(Icons.arrow_forward_ios),
+  //         );
+  //       },
+  //     ),
+  //   );
   }
 }
