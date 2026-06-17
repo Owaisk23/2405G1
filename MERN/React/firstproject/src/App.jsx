90cc5cbf0d6c0f9d9ceb3eed3d32af0593606e00
owaisk23 <owais1566@gmail.com>
2026-06-17 20:35:42 +0500

    added Navbar bootstrap

---
+++ MERN/React/firstproject/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import MyName from './MyName'
+import Navbar from './Navbar'
+
+function App() {
+  
+  return (
+    <div>
+      <Navbar />
+      <h1 className='heading'>UBAID ULLAH!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
+      <p className='bg-warning'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem nam ut in quo eligendi voluptate aliquam corporis libero architecto itaque numquam voluptatem nesciunt expedita id nisi repudiandae praesentium rerum sequi, quia neque dolor quaerat vero sed. Est blanditiis, impedit aliquid pariatur harum eos eius, enim maiores labore nemo qui!</p>
+      <MyName />
+   </div>
+  )
+}
+
+export default App
---
+++ MERN/React/firstproject/src/MyName.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyName = () => {
+  return (
+      <div>
+        <h1 className='heading'>My Name is UBAID ULLAH SAUDIA WALA</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quibusdam, ab cumque illo, illum minima laboriosam numquam sequi maiores dignissimos vitae laudantium assumenda corporis eveniet quam quidem alias neque odit, id tempore consequatur consequuntur maxime inventore. Exercitationem vel officia fuga, iusto reiciendis numquam. Laborum facilis alias molestiae nobis animi assumenda velit totam aliquid illum in, iure ab consequuntur nihil. Iusto tenetur minus possimus numquam officiis quo doloremque ut at qui rerum tempore molestias ipsa provident repellat, cumque a odio asperiores, obcaecati soluta vel recusandae quis quasi exercitationem. Amet placeat sunt, corporis minus sit voluptatem atque. Excepturi numquam quos eligendi porro ullam! Sint a voluptatum autem quis impedit nam. Aliquam, nulla aliquid numquam eligendi voluptatibus vel expedita enim, pariatur necessitatibus natus non cupiditate amet vitae odit. Perspiciatis vel pariatur corporis harum ullam itaque debitis enim alias tempore amet nam, praesentium ipsa eaque, officiis expedita velit totam facilis quos. Quidem, dicta accusamus.</p>
+
+    </div>
+  )
+}
+
+export default MyName
