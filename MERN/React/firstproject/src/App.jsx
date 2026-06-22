f544bf789a178464498de57317122c4f8673e043
owaisk23 <owais1566@gmail.com>
2026-06-22 20:48:09 +0500

    added props

--- MERN/React/firstproject/src/App.jsx
+++ MERN/React/firstproject/src/App.jsx
@@ -3,16 +3,21 @@ import './App.css'
 import Navbar from './components/Navbar'
 import MyName from './components/MyName'
 import Footer from './components/Footer'
+import Bachra from './assets/bachra.jpg'
 
 function App() {
   
   return (
     <div>
       <Navbar />
-      <h1 className='heading'>UBAID ULLAH!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
-      <p className='bg-warning'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem nam ut in quo eligendi voluptate aliquam corporis libero architecto itaque numquam voluptatem nesciunt expedita id nisi repudiandae praesentium rerum sequi, quia neque dolor quaerat vero sed. Est blanditiis, impedit aliquid pariatur harum eos eius, enim maiores labore nemo qui!</p>
-      <MyName />
+      {/* <h1 className='heading'>UBAID ULLAH!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
+      <p className='bg-warning'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem nam ut in quo eligendi voluptate aliquam corporis libero architecto itaque numquam voluptatem nesciunt expedita id nisi repudiandae praesentium rerum sequi, quia neque dolor quaerat vero sed. Est blanditiis, impedit aliquid pariatur harum eos eius, enim maiores labore nemo qui!</p> */}
+      <MyName name={"UBAID ULLAH "} profession={"State Agent"} />
+      <MyName name={"Ramiz"} profession={"INVIGILATOR "} />   
+      <MyName name={"Subhan"} profession={"Khaadi Outlet"} />   
 
+      <img src={Bachra} alt="" />
+      <img src="https://res.cloudinary.com/dnfiycc46/image/upload/v1764047078/2309B2/o7tqhkv6wgvktval6epd.jpg" alt="" />
       <Footer />
    </div>
   )
--- MERN/React/firstproject/src/components/MyName/index.jsx
+++ MERN/React/firstproject/src/components/MyName/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const index = () => {
+const index = (props) => {
   return (
      <div>
-        <h1 className='heading'>My Name is UBAID ULLAH SAUDIA WALA</h1>
+        <h1 className='heading'>My Name is {props.name}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quibusdam, ab cumque illo, illum minima laboriosam numquam sequi maiores dignissimos vitae laudantium assumenda corporis eveniet quam quidem alias neque odit, id tempore consequatur consequuntur maxime inventore. Exercitationem vel officia fuga, iusto reiciendis numquam. Laborum facilis alias molestiae nobis animi assumenda velit totam aliquid illum in, iure ab consequuntur nihil. Iusto tenetur minus possimus numquam officiis quo doloremque ut at qui rerum tempore molestias ipsa provident repellat, cumque a odio asperiores, obcaecati soluta vel recusandae quis quasi exercitationem. Amet placeat sunt, corporis minus sit voluptatem atque. Excepturi numquam quos eligendi porro ullam! Sint a voluptatum autem quis impedit nam. Aliquam, nulla aliquid numquam eligendi voluptatibus vel expedita enim, pariatur necessitatibus natus non cupiditate amet vitae odit. Perspiciatis vel pariatur corporis harum ullam itaque debitis enim alias tempore amet nam, praesentium ipsa eaque, officiis expedita velit totam facilis quos. Quidem, dicta accusamus.</p>
-
+        <h2>PROFESSION: {props.profession}</h2>
     </div>
   )
 }
