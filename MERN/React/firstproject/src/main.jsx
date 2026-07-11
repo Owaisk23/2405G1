617a5f1cedd6bfa189bb520c66f3d88adf373b88
owaisk23 <owais1566@gmail.com>
2026-07-11 16:26:15 +0500

    added useEffect hook

--- MERN/React/firstproject/src/main.jsx
+++ MERN/React/firstproject/src/main.jsx
@@ -9,5 +9,5 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+   </StrictMode>,
 )
